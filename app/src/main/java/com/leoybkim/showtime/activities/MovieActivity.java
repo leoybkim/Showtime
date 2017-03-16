2ebfb25ce86ada45d3b066c22a1554e86cfaa0d4
leoybkim <leoybkim@gmail.com>
2017-03-16 17:15:37 -0400

    Refactor MovieActivity and MovieDetailsActivity 1. Add swipe refresh layout to movie list view items 2. Update youtube fragments to youtube base activity for vertical orientations

--- app/src/main/java/com/leoybkim/showtime/activities/MovieActivity.java
+++ app/src/main/java/com/leoybkim/showtime/activities/MovieActivity.java
@@ -2,6 +2,7 @@ package com.leoybkim.showtime.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,34 +23,80 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
+import static com.leoybkim.showtime.R.id.swipeContainer;
+
 public class MovieActivity extends AppCompatActivity {
 
-    ArrayList<Movie> movies;
-    MovieArrayAdapter movieArrayAdapter;
-    ListView lvItems;
+    private static final String LOG_TAG = MovieActivity.class.getSimpleName();
+    private ArrayList<Movie> mMovies;
+    private MovieArrayAdapter mMovieArrayAdapter;
+    private ListView mListViewItems;
+    private String nowPlayingUrl;
+    private SwipeRefreshLayout mSwipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
 
-        lvItems = (ListView) findViewById(R.id.lvmovies);
-        movies = new ArrayList<>();
-        movieArrayAdapter = new MovieArrayAdapter(this, movies);
-        lvItems.setAdapter(movieArrayAdapter);
+        // Find view resources
+        mSwipeContainer = (SwipeRefreshLayout) findViewById(swipeContainer);
+        mListViewItems = (ListView) findViewById(R.id.lvmovies);
 
+        // Setup custom adapter
+        mMovies = new ArrayList<>();
+        mMovieArrayAdapter = new MovieArrayAdapter(this, mMovies);
+        mListViewItems.setAdapter(mMovieArrayAdapter);
+
+        // Setup API and make http request
         String movieDbApiKey = BuildConfig.MOVIE_DB_API_KEY;
-        String nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + movieDbApiKey;
+        nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=" + movieDbApiKey;
+        getNowPlaying();
 
+        // Launch MovieDetailsActivity on click
+        mListViewItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Movie movie = mMovieArrayAdapter.getItem(i);
+                Intent intent = new Intent(MovieActivity.this, MovieDetailsActivity.class);
+                intent.putExtra("movie", movie);
+                startActivity(intent);
+            }
+        });
+
+        // Setup refresh listener which triggers new data loading
+        mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getNowPlaying();
+                mSwipeContainer.setRefreshing(false);
+            }
+        });
+
+        // Configure the refreshing colors
+        mSwipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    // Make API call to TheMovieDatabase and retrieves "now playing" movies in json object
+    public void getNowPlaying() {
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(nowPlayingUrl, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray movieJsonResults = null;
                 try {
+                    // Clear pre-existing array list to avoid duplication
+                    if (mMovies != null) { mMovies.clear(); }
+
+                    // Parse json results
                     movieJsonResults = response.getJSONArray("results");
-                    movies.addAll(Movie.fromJSONArray(movieJsonResults));
-                    movieArrayAdapter.notifyDataSetChanged();
+                    mMovies.addAll(Movie.fromJSONArray(movieJsonResults));
+
+                    // Update adapter
+                    mMovieArrayAdapter.notifyDataSetChanged();
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -58,16 +105,6 @@ public class MovieActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
-        });
-
-        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Movie movie = movieArrayAdapter.getItem(i);
-                Intent intent = new Intent(MovieActivity.this, MovieDetailsActivity.class);
-                intent.putExtra("movie", movie);
-                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/leoybkim/showtime/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/leoybkim/showtime/activities/MovieDetailsActivity.java
@@ -1,7 +1,8 @@
 package com.leoybkim.showtime.activities;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,9 +10,10 @@ import android.widget.ProgressBar;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
-import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerView;
 import com.leoybkim.showtime.BuildConfig;
 import com.leoybkim.showtime.R;
 import com.leoybkim.showtime.models.Movie;
@@ -20,7 +22,6 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,44 +35,29 @@ import static com.leoybkim.showtime.R.id.progress_bar;
  * Created by leo on 12/03/17.
  */
 
-public class MovieDetailsActivity extends AppCompatActivity {
+public class MovieDetailsActivity extends YouTubeBaseActivity {
 
     public static final String LOG_TAG = MovieDetailsActivity.class.getSimpleName();
-    private String videoId;
+    private String mVideoId;
+    private YouTubePlayerView mYoutubePlayerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
 
-        // Prepare api call
+        // Prepare API call
         String movieDbApiKey = BuildConfig.MOVIE_DB_API_KEY;
         Movie movie = getIntent().getParcelableExtra("movie");
         String movieUrl = "https://api.themoviedb.org/3/movie/" + movie.getId() + "/videos?api_key=" + movieDbApiKey;
 
-        ImageView backdrop = (ImageView) findViewById(R.id.backdrop);
-        ImageView playButton = (ImageView) findViewById(R.id.play_button);
+        // Setup layout
         TextView title = (TextView) findViewById(R.id.title);
         TextView overview = (TextView) findViewById(R.id.overview);
         TextView releaseDate = (TextView) findViewById(R.id.release_date);
         RatingBar ratings = (RatingBar) findViewById(R.id.rating_star);
-        final ProgressBar progressBar = (ProgressBar) findViewById(progress_bar);
-        final YouTubePlayerFragment youtubeFragment = (YouTubePlayerFragment)
-                getFragmentManager().findFragmentById(R.id.youtube_fragment);
-
-        // Load backdrop image
-        Picasso.with(this).load(movie.getWideBackdropPath())
-                .transform(new RoundedCornersTransformation(30, 10)).into(backdrop, new Callback() {
-            @Override
-            public void onSuccess() {
-                progressBar.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onError() {
-                // TODO Auto-generated method stub
-            }
-        });
+        mYoutubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        int orientation = getResources().getConfiguration().orientation;
 
         // Load movie details
         title.setText(movie.getOriginalTitle());
@@ -84,10 +70,10 @@ public class MovieDetailsActivity extends AppCompatActivity {
         client.get(movieUrl, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                JSONArray movieJsonResults = null;
                 try {
-                    movieJsonResults = response.getJSONArray("results");
-                    videoId = movieJsonResults.getJSONObject(0).getString("key").toString();
+                    // Retrieve video url from the movie specified
+                    // TODO: check if it has a trailer, if it is from Youtube
+                    mVideoId = response.getJSONArray("results").getJSONObject(0).getString("key");
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -99,27 +85,53 @@ public class MovieDetailsActivity extends AppCompatActivity {
             }
         });
 
-        // Start youtube fragment on image touch
-        playButton.setOnTouchListener(new View.OnTouchListener() {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            // Horizontal orientation
+            ImageView backdrop = (ImageView) findViewById(R.id.backdrop);
+            ImageView playButton = (ImageView) findViewById(R.id.play_button);
+            final ProgressBar progressBar = (ProgressBar) findViewById(progress_bar);
+
+            // Load backdrop image
+            Picasso.with(this).load(movie.getWideBackdropPath())
+                    .transform(new RoundedCornersTransformation(30, 10)).into(backdrop, new Callback() {
+                @Override
+                public void onSuccess() {
+                    progressBar.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onError() {
+                    // TODO Auto-generated method stub
+                }
+            });
+
+            // Start youtube fragment on image touch
+            playButton.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    mYoutubePlayerView.setVisibility(View.VISIBLE);
+                    initializeYoutubePlayer();
+                    return false;
+                }
+            });
+        } else {
+            // Vertical orientation
+            initializeYoutubePlayer();
+        }
+    }
+
+    // Cue trailers
+    public void initializeYoutubePlayer() {
+        mYoutubePlayerView.initialize(BuildConfig.YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.cueVideo(mVideoId);
+            }
 
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                youtubeFragment.initialize(com.leoybkim.showtime.BuildConfig.YOUTUBE_API_KEY,
-                        new YouTubePlayer.OnInitializedListener() {
-                            @Override
-                            public void onInitializationSuccess(YouTubePlayer.Provider provider,
-                                                                YouTubePlayer youTubePlayer, boolean b) {
-                                youTubePlayer.loadVideo(videoId);
-                            }
-                            @Override
-                            public void onInitializationFailure(YouTubePlayer.Provider provider,
-                                                                YouTubeInitializationResult youTubeInitializationResult) {
-
-                            }
-                        });
-                return false;
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                Log.d(LOG_TAG, youTubeInitializationResult.toString());
             }
         });
-
     }
 }
